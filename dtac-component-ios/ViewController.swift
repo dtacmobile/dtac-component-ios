7977bb99698ac99042d23857a7809214b1153e8b
akehirasawa <akehirasawa@gmail.com>
2020-01-16 14:54:00 +0700

    Initial Commit

---
+++ dtac-component-ios/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  dtac-component-ios
+//
+//  Created by Akebordin Duangphoommes on 16/1/20.
+//  Copyright © 2020 Akebordin Duangphoommes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
