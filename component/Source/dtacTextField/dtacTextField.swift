a2cb6aff767fbda14bdaefe3bb0fafdbab5dc1b9
CHAKRIT PANIAM <Chakrit69@gmail.com>
2021-04-22 16:11:59 +0700

    update pod  spec 0.7.0

--- component/Source/dtacTextField/dtacTextField.swift
+++ component/Source/dtacTextField/dtacTextField.swift
@@ -18,18 +18,19 @@ open class dtacTextField: UITextField {
     
     let padding = UIEdgeInsets(top: 0, left: 10, bottom: 0, right: 10)
     
-    override open func textRect(forBounds bounds: CGRect) -> CGRect {
-        return bounds.inset(by: padding)
-    }
-    
-    override open func placeholderRect(forBounds bounds: CGRect) -> CGRect {
-        return bounds.inset(by: padding)
-    }
-    
-    override open func editingRect(forBounds bounds: CGRect) -> CGRect {
-        return bounds.inset(by: padding)
-    }
-    
+//    override open func textRect(forBounds bounds: CGRect) -> CGRect {
+//        return bounds.insetBy(dx: <#T##CGFloat#>, dy: <#T##CGFloat#>)
+//    }
+//    
+//    override open func placeholderRect(forBounds bounds: CGRect) -> CGRect {
+//        
+//        return bounds.insetBy(dx: padding, dy: padding)
+//    }
+//    
+//    override open func editingRect(forBounds bounds: CGRect) -> CGRect {
+//        return bounds.insetBy(dx: padding, dy: padding)
+//    }
+//    
     func setup() {
         self.layer.borderColor = UIColor.shadow.cgColor
         self.layer.borderWidth = 1
